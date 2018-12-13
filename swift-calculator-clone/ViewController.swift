69b090acffd316f026e694df7580e5bec76b5a57
rogercodes1 <rogerperez.us@gmail.com>
2018-12-13 14:38:23 -0800

    First swift commit

---
+++ swift-calculator-clone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swift-calculator-clone
+//
+//  Created by Roger Perez on 12/13/18.
+//  Copyright © 2018 Roger Perez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
