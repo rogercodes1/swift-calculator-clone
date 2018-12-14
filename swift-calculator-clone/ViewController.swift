f9d35fe0ebdff268b71aefd933b34c8818c07bdc
rogercodes1 <rogerperez.us@gmail.com>
2018-12-14 14:57:27 -0800

    foundation of app complete;

--- swift-calculator-clone/ViewController.swift
+++ swift-calculator-clone/ViewController.swift
@@ -8,11 +8,53 @@
 
 import UIKit
 
+enum modes {
+    case not_set
+    case addition
+    case subtraction
+    
+}
+
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var label: UILabel!
+    
+    var labelString:String = "0"
+    var currentMode:modes = .not_set
+    var savedNum:Int = 0
+    var lastButtonWasMode:Bool = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    @IBAction func didPressPlus(_ sender: Any) {
+    }
+    
+    @IBAction func didPressSubtraction(_ sender: Any) {
+    }
+    
+    @IBAction func didPressMultiplication(_ sender: Any) {
+    }
+    
+    @IBAction func didPressEquals(_ sender: Any) {
+    }
+    
+    @IBAction func didPressClear(_ sender: Any) {
+    }
+    
+    @IBAction func didPressNumber(_ sender: UIButton) {
+    }
+    
+    
+    
+    func updateText(){
+        
+    }
+  
+    
+    func changeModes(newMode:modes){
+        
     }
 
 
